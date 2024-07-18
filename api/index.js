4eb7d24566e276d7797e522c4b34d934a9b92e9f
Tyson Potter <tysonpottersd@gmail.com>
2024-07-18 16:39:14 -0600

    switched database to MYSQL

--- api/index.js
+++ api/index.js
@@ -1,46 +1,45 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const app = express();
 const port = 3000;
-
-// Open SQLite database
-const dbPath = path.resolve(__dirname, "../database/mydatabase.db");
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error("Error opening database:", err.message);
-  } else {
-    console.log("Connected to the SQLite database.");
-  }
+const dotenv = require("dotenv");
+// Open mySQL database
+require("dotenv").config();
+const mysql = require("mysql2");
+//login commands
+//sudo mysql.server start
+//mysql -u root -p
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD, // Your MySQL root password
+  database: "chessDataBase",
+  port: process.env.PORT, // Ensure this matches your MySQL port
 });
 
-app.use(express.json());
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    return;
+  }
+  console.log("Connected to the database as id " + connection.threadId);
 
-// app.post("/users", (req, res) => {
-//   const { name, email } = req.body;
-//   db.run(
-//     "INSERT INTO users (name, email) VALUES (?, ?)",
-//     [name, email],
-//     function (err) {
-//       if (err) {
-//         res.status(500).json({ error: err.message });
-//         return;
-//       }
-//       res.status(201).json({ user_id: this.lastID });
-//     }
-//   );
-// });
+  // Query the users table
+  connection.query("SELECT * FROM users", (error, results, fields) => {
+    if (error) throw error;
+    console.log("Users:", results);
 
-process.on("SIGINT", () => {
-  db.close((err) => {
-    if (err) {
-      console.error("Error closing database:", err.message);
-    } else {
-      console.log("Closed the database connection.");
-    }
-    process.exit(0);
+    // Close the connection
+    connection.end((err) => {
+      if (err) {
+        console.error("Error ending the connection:", err.stack);
+        return;
+      }
+      console.log("Connection closed");
+    });
   });
 });
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
