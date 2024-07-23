33555e3f2e9020725f586708ffa42b60cf7bbe0e
Tyson Potter <tysonpottersd@gmail.com>
2024-07-23 17:13:53 -0600

    made board

--- frontend/chessReact/src/App.jsx
+++ frontend/chessReact/src/App.jsx
@@ -1,14 +1,25 @@
-
+import Board from './components/Board';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+//create set state variable
+const [gameState, setGameState] = useState(true);
+
 
 
   return (
     <>
-   
+
+      {gameState ? (
+        <Board gameState={gameState}/>
+      ) : (
+   null
+      )}
     </>
-  )
+  );
 }
+
+
 
 export default App
