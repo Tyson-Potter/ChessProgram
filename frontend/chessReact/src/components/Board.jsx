11a1749fa9dc19a1ff21874a90c27627980854fa
Tyson Potter <tysonpottersd@gmail.com>
2024-07-25 12:16:47 -0600

    Added pieaces to board

--- frontend/chessReact/src/components/Board.jsx
+++ frontend/chessReact/src/components/Board.jsx
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/board.css';
+import blackBishop from '/src/assets/images/black-bishop.png';
+import blackKing from '/src/assets/images/black-king.png';
+import blackKnight from '/src/assets/images/black-knight.png';
+import blackPawn from '/src/assets/images/black-pawn.png';
+import blackQueen from '/src/assets/images/black-queen.png';
+import blackRook from '/src/assets/images/black-rook.png';
+import whiteBishop from '/src/assets/images/white-bishop.png';
+import whiteKing from '/src/assets/images/white-king.png';
+import whiteKnight from '/src/assets/images/white-knight.png';
+import whitePawn from '/src/assets/images/white-pawn.png';
+import whiteQueen from '/src/assets/images/white-queen.png';
+import whiteRook from '/src/assets/images/white-rook.png';
 
 function Board(gameState) {
     // Create set state variable
@@ -8,7 +20,9 @@ function Board(gameState) {
     // Set gameState to true on component mount
    
 
-    const board = [
+
+   
+    let board = [
         {"x": 0, "y": 7, "color": "white"},
         {"x": 1, "y": 7, "color": "black"},
         {"x": 2, "y": 7, "color": "white"},
@@ -74,7 +88,59 @@ function Board(gameState) {
         {"x": 6, "y": 0, "color": "black"},
         {"x": 7, "y": 0, "color": "white"}
     ];
-
+   board  = board.reverse();
+    const pieces = [
+        { piece: 'whiteRook', x: 0, y: 0, hasMoved: false },
+        { piece: 'whiteKnight', x: 1, y: 0 },
+        { piece: 'whiteBishop', x: 2, y: 0 },
+        { piece: 'whiteQueen', x: 3, y: 0 },
+        { piece: 'whiteKing', x: 4, y: 0, hasMoved: false },
+        { piece: 'whiteBishop', x: 5, y: 0 },
+        { piece: 'whiteKnight', x: 6, y: 0 },
+        { piece: 'whiteRook', x: 7, y: 0, hasMoved: false },
+        { piece: 'whitePawn', x: 0, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 1, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 2, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 3, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 4, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 5, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 6, y: 1, hasMoved: false },
+        { piece: 'whitePawn', x: 7, y: 1, hasMoved: false },
+        { piece: 'blackRook', x: 0, y: 7, hasMoved: false },
+        { piece: 'blackKnight', x: 1, y: 7 },
+        { piece: 'blackBishop', x: 2, y: 7 },
+        { piece: 'blackQueen', x: 3, y: 7 },
+        { piece: 'blackKing', x: 4, y: 7, hasMoved: false },
+        { piece: 'blackBishop', x: 5, y: 7 },
+        { piece: 'blackKnight', x: 6, y: 7 },
+        { piece: 'blackRook', x: 7, y: 7, hasMoved: false },
+        { piece: 'blackPawn', x: 0, y: 6, hasMoved: false },
+        { piece: 'blackPawn', x: 1, y: 6, hasMoved: false },
+        { piece: 'blackPawn', x: 2, y: 6, hasMoved: false },
+        { piece: 'blackPawn', x: 3, y: 6, hasMoved: false },
+        { piece: 'blackPawn', x: 4, y: 6, hasMoved: false },
+        { piece: 'blackPawn', x: 5, y: 4, hasMoved: false },
+        { piece: 'blackPawn', x: 6, y: 6, hasMoved: false },
+        { piece: 'blackPawn', x: 7, y: 6, hasMoved: false },
+    ];
+    const pieceImages = {
+        blackBishop: blackBishop,
+        blackKing: blackKing,
+        blackKnight: blackKnight,
+        blackPawn: blackPawn,
+        blackQueen: blackQueen,
+        blackRook: blackRook,
+        whiteBishop: whiteBishop,
+        whiteKing: whiteKing,
+        whiteKnight: whiteKnight,
+        whitePawn: whitePawn,
+        whiteQueen: whiteQueen,
+        whiteRook: whiteRook,
+    };
+    const getPieceAtPosition = (x, y) => {
+        const piece = pieces.find(piece => piece.x === x && piece.y === y);
+        return piece ? piece.piece : null;
+    };
     return (
         <div className='board-container'>
         <div className="grid">
@@ -83,7 +149,7 @@ function Board(gameState) {
                     key={`${square.x},${square.y}`}
                     id={`${square.x},${square.y}`} 
                     className={`${square.color} cell`}
-                ></button>
+                >{getPieceAtPosition(square.x, square.y) ? <img src={pieceImages[getPieceAtPosition(square.x, square.y)]} alt={getPieceAtPosition(square.x, square.y)} /> : null}</button>
             ))}
         </div>
         </div>
