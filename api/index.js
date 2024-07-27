02e6c6fd8a217b8427672a7761fbc8de60eaf36a
Tyson Potter <tysonpottersd@gmail.com>
2024-07-27 13:04:23 -0600

    changed database to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,45 +1,53 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-const port = 3000;
+
 const dotenv = require("dotenv");
-// Open mySQL database
+
 require("dotenv").config();
-const mysql = require("mysql2");
-//login commands
-//sudo mysql.server start
-//mysql -u root -p
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.DB_PASSWORD, // Your MySQL root password
-  database: "chessDataBase",
-  port: process.env.PORT, // Ensure this matches your MySQL port
-});
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.stack);
-    return;
-  }
-  console.log("Connected to the database as id " + connection.threadId);
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT;
+const uri = process.env.uri;
 
-  // Query the users table
-  connection.query("SELECT * FROM users", (error, results, fields) => {
-    if (error) throw error;
-    console.log("Users:", results);
+// Middleware to parse JSON bodies
+app.use(express.json());
+// MongoDB connection URI
 
-    // Close the connection
-    connection.end((err) => {
-      if (err) {
-        console.error("Error ending the connection:", err.stack);
-        return;
-      }
-      console.log("Connection closed");
+// MongoDB client
+
+const client = new MongoClient(uri, {});
+
+let collection;
+
+// Connect to MongoDB and select the collection
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    const database = client.db("ChessAppDataBase"); // Replace with your database name
+    collection = database.collection("games"); // Replace with your collection name
+    console.log("Connected to MongoDB");
+
+    // Start the server after the connection is established
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
     });
-  });
-});
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+connectToDatabase();
+
+//End Points
+
+app.post("/CreateGame", async (req, res) => {
+  try {
+    const doc = req.body;
+    const result = await collection.insertOne(doc);
+    res.status(201).json({ insertedId: result.insertedId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to insert document" });
+  }
 });
