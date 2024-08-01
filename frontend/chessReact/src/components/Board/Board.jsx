0ca5b76b1a56a8db74520ac72faa0729fdee23b9
Tyson Potter <tysonpottersd@gmail.com>
2024-08-01 13:01:42 -0600

    added parital movment capability and added field

--- frontend/chessReact/src/components/Board/Board.jsx
+++ frontend/chessReact/src/components/Board/Board.jsx
@@ -15,9 +15,12 @@ import whiteQueen from "/src/assets/images/white-queen.png";
 import whiteRook from "/src/assets/images/white-rook.png";
 
 function Board(gameState) {
+  const [selectedPiece, setSelectedPiece] = useState(null);
+  const [selectedSquare, setSelectedSquare] = useState(null);
   if (gameState != false) {
     let board = gameState.gameState.board;
     let pieces = gameState.gameState.piecePostions;
+     
     const pieceImages = {
       blackBishop: blackBishop,
       blackKing: blackKing,
@@ -32,6 +35,53 @@ function Board(gameState) {
       whiteQueen: whiteQueen,
       whiteRook: whiteRook,
     };
+   
+const handleClick = (square) => {
+  // if not current players turn do nothing
+  if(gameState.gameState.currentTurn != localStorage.getItem("color")){
+
+   console.log("Not your turn");
+  }else{
+    //Check if Clicked square has a pieace on it 
+    const clickedPiece = pieces.find(obj => obj.x === square.x && obj.y === square.y);
+    if(clickedPiece){
+      
+      //Check if they clicked thier piece or an enempy piece
+      if(clickedPiece.color===localStorage.getItem("color")){
+        console.log("clicked thier Piece")
+        //add css stuff to change what selected square looks like border or somthing
+        setSelectedPiece(clickedPiece);
+       setSelectedSquare(null);
+      
+      }else{
+        console.log("clicked enemy Piece")
+        //clicked an ememy Piece
+        if(selectedPiece!=null){
+          console.log("Make Move attack");
+          //sent api request to make a move
+              //reset selected Pieace and selected square
+        }else{
+          //Do Nothing
+        }
+      }
+
+     
+    }else{
+      //clicked an empty square
+      if(selectedPiece!=null){
+        console.log("Make Move");
+        
+        //sent api request to make a move
+        //reset selected Pieace and selected square
+      }else{
+        //Do Nothing
+      }
+      
+    }
+
+  }
+};
+    
 
     if (localStorage.getItem("color") !== "white") {
       board = [...board].reverse();
@@ -47,7 +97,7 @@ function Board(gameState) {
       <div className="board-container">
         <div className="grid">
           {board.map((square) => (
-            <button
+            <button  onClick={() => handleClick(square)}
               key={`${square.x},${square.y}`}
               id={`${square.x},${square.y}`}
               className={`${square.color} cell`}
